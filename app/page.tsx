a5700d6d0e393f1efdecb2b7b65e3ae9cc538979
Mirza Raheel <raheel1195@gmail.com>
2025-10-31 18:48:47 +0500

    added console statement

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { notFound } from 'next/navigation';
 import { Post } from "@/types/post";
 
 async function getPosts() {
+  console.log("🔍 NEXT_PUBLIC_BASE_URL =", process.env.NEXT_PUBLIC_BASE_URL);
+
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`, { cache: 'no-store' });
   const data = await res.json();
   const posts = data.posts;
